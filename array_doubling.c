7d51439aad11269755aa09ba7b22c537ab0be542
= <bwheatman@gmail.com>
2017-11-07 16:57:18 -0500

    basic implementation of linked list and table doubling without delete

---
+++ array_doubling.c
@@ -0,0 +1,99 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#define MIN_INT (-2147483648)
+
+
+
+typedef struct _list {
+	int size;
+	int capacity;
+	int* items;
+} list_t;
+
+
+
+int max(list_t *list) {
+	int current_max = MIN_INT;
+	for (int i = 0; i < list->size; i++) {
+		if (list->items[i] > current_max) {
+			current_max = list->items[i];
+		}
+	}
+	return current_max;
+}
+
+void slide_right(list_t *list, int index) {
+	int el = list->items[index];
+	while (index < list->size) {
+		int temp = list->items[index];
+		list->items[index++] = el;
+		el = temp;
+	}
+	list->items[index] = el;
+}
+
+
+void insert(list_t *list, int index, int elem) {
+	if (index > list->size) {
+		index = list->size;
+	}
+	if (list->size == list->capacity) {
+		int * new_items = (int*) malloc(list->capacity*2*sizeof(*(list->items)));
+		list->capacity*=2;
+		int i = 0;
+		for (; i < index; i++) {
+			new_items[i] = list->items[i];
+		}
+		new_items[i] = elem;
+		for (; i<list->size; i++) {
+			new_items[i+1] = list->items[i];
+		}
+		free(list->items);
+		list->items = new_items;
+		list->size+=1;
+
+	} else {
+		if (index < list->size) {
+			slide_right(list, index);
+		}
+		list->size++;
+		list->items[index] = elem;	
+	}
+
+}
+
+void print_array(list_t *list) {
+	for (int i = 0; i < list->size; i++) {
+		printf("%d ", list->items[i]);
+	}
+	printf("\n\n");
+}
+
+void setup(list_t *list){
+	list->size = 0;
+	list->capacity = 8;
+	list->items = (int*) malloc(8*sizeof(*(list->items)));
+	for (int i = 0; i < list->capacity; i++) {
+		list->items[i] = 0;
+	}
+}
+
+int main(){
+	list_t list;
+	setup(&list);
+	print_array(&list);
+	while (1) {
+		printf("loc =");
+		int loc;
+		scanf("%d", &loc);
+		int elem;
+		printf("elem =");
+		scanf("%d", &elem);
+		insert(&list, loc, elem);
+		print_array(&list);
+	}
+
+
+}
+
