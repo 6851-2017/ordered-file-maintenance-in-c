d7218606a4e05c7ddafa6d19e358d025b1beda6a
= <bwheatman@gmail.com>
2017-11-07 18:27:25 -0500

    linked list working with add and delete here

--- linked_list.c
+++ linked_list.c
@@ -6,73 +6,119 @@
 
 
 typedef struct _node {
-	_node *prev;
-	_node *next;
+	struct _node *prev;
+	struct _node *next;
 	int val;
+} node_t;
+
+typedef struct _list {
+	node_t *items;
 } list_t;
+
 
 
 int max(list_t *list) {
 	int current_max = MIN_INT;
-	while (list) {
-		if (list->val > current_max) {
-			current_max = list->val;
+	node_t *node =  list->items;
+	while (node) {
+		if (node->val > current_max) {
+			current_max = node->val;
 		}
-		list = list->next;
+		node = node->next;
 	}
 	return current_max;
 }
 
+// if index is greater than the length of the list this returns last element
+node_t* get(list_t *list, int index) {
+	if (!(list->items)) {
+		return NULL;
+	}
+	node_t *node =  list->items;
+	for (int i = 0; i < index; i++) {
+		if (node->next == NULL) {
+			return node;
+		}
+		node = node->next;
+	}
+	return node;
+}
 
-void insert(list_t *list, int index, int elem) {
+void insert_here(list_t *list, node_t *node, int elem) {
 	// if the list is empty
-	if (!list) {
-		list = (*list_t) malloc(sizeof(*list));
-		list->val = elem;
-		list->next = null;
+	if (!node) {
+		node = (node_t*) malloc(sizeof(*node));
+		node->val = elem;
+		node->next = list->items;
+		node->prev = NULL;
+		list->items = node;
 		return;
 	}
-	for (int i = 0; i < index; i++) {
-		if (list->next == null) {
-			list->next = (*list_t) malloc(sizeof(*list));
-			list->next->val = elem;
-			list->next->prev = list;
-			list->next->next = null;
-			return;
-		}
-		list = list->next;
-	}
-	list_t *new = (*list_t) malloc(sizeof(*list));
-	new->prev = list->prev;
-	new->next = list;
+	node_t *new = (node_t*) malloc(sizeof(*node));
+	new->prev = node->prev;
+	new->next = node;
 	new->val = elem;
-	list->prev->next = new;
+	if (node->prev == NULL) {
+		list->items = new;
+	} else {
+		node->prev->next = new;
+	}
+	node->prev = new;
+}
 
+void delete_here(list_t *list, node_t *node) {
+	// if the list is empty
+	if (!(list->items)) {
+		return;
+	}
+	if (node->next) {
+		node->next->prev = node->prev;
+	}
+	if (node->prev) {
+		node->prev->next = node->next;
+		free(node);
+		return;
+	}
+	list->items = node->next;
+	free(node);
+
+	
 }
 
 void print_array(list_t *list) {
-	while (list) {
-		printf("%d ", list->val);
-		list = list->next;
+	node_t *node =  list->items;
+	while (node) {
+		printf("%d ", node->val);
+		node = node->next;
 	}
 	printf("\n\n");
 }
 
 void setup(list_t *list){
+	list->items = NULL;
 }
 
 int main(){
 	list_t list;
 	setup(&list);
 	print_array(&list);
-	while (1) {
+	for (int i = 0; i < 10; i++) {
 		printf("loc =");
 		int loc;
 		scanf("%d", &loc);
 		int elem;
 		printf("elem =");
 		scanf("%d", &elem);
-		insert(&list, loc, elem);
+		node_t *node = get(&list, loc);
+		insert_here(&list, node, elem);
+		print_array(&list);
+	}
+	for (int i = 0; i < 10; i++) {
+		printf("loc =");
+		int loc;
+		scanf("%d", &loc);
+		node_t *node = get(&list, loc);
+		delete_here(&list, node);
 		print_array(&list);
 	}
 
