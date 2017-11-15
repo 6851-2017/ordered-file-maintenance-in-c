324413e0dac584ecd9ef2cf0c5c4e653ea6ad7da
= <bwheatman@gmail.com>
2017-11-14 21:05:44 -0500

    doubling for ofm working

--- ofm-testing.c
+++ ofm-testing.c
@@ -4,7 +4,6 @@
 // inserts N elements sequentially into ofm.
 // prints out time it takes for N operations
 void testSequentialInsertOFM(list_t* list, int size){
-	setup(list);
 
 	printf("Initial List: \n");
 	print_array(list);
@@ -15,8 +14,9 @@ void testSequentialInsertOFM(list_t* list, int size){
 	start = clock();
 	insert_first(list, 0);
 	int* item = get_first(list);
-	for(int i=0; i<size; i++){
+	for(int i=0; i<size-1; i++){
 		item = insert_after(list, i+1, item);
+		//print_array(list);
 	}
 	end = clock();
 
@@ -28,7 +28,7 @@ void testSequentialInsertOFM(list_t* list, int size){
 // inserts N elements at random locations into OFM. 
 // prints out itme it takes for N operations
 void testRandomInsertOFM(list_t* list, int size){
-
+	/*
 	int randomInsert[N_0] = {0};
 	int index;
 	for(int i=0; i< size; i++){
@@ -52,12 +52,14 @@ void testRandomInsertOFM(list_t* list, int size){
 	cpu_time_used = ((double) (end - start)) / CLOCKS_PER_SEC;
 	print_array(list);
 	printf("%d random insertions, time: %f \n", size, cpu_time_used);
+	*/
 }
 
 int main(){
 	list_t list;
-	printf("Beginning Testing Suite. N is: %d\n", N_0);
-	testSequentialInsertOFM(&list, 3*N_0/4);
+	setup(&list);
+	printf("Beginning Testing Suite. N is: %d\n", list.N);
+	testSequentialInsertOFM(&list, 82);
 	//testRandomInsertOFM(N/4);	
 
 }
